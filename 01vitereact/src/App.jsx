af49a1e60389862b646a97f0cb97790c9d78e9b6
Kirantevaratti341265 <kiranpatil.kp1303@gmail.com>
2023-12-23 08:33:35 +0530

    packagejson

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+function App() {
+  const username = "chai aur code"
+
+
+  return (
+    <>
+   <Chai />
+   <h1>chai aur react {username} </h1>
+   <p>test para</p>
+   </>
+   
+  )
+}
+
+export default App
---
+++ interview-discussion/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useEffect } from 'react'
+
+function App() {
+  console.log('app rendered', Math.random())
+  const [value, setValue] = useState({
+    vlaue: 0,
+  })
+
+
+
+  // // const [multipliedValue, setMultipliedValue] = useState(1)
+  // let multipliedValue = value * 5
+
+  // const multiplybyfive = () => {
+  //   // setMultipliedValue(value * 5)
+  //   setValue(value + 1)
+
+  // }
+
+  const clickMe = () => {
+    setValue({
+      value: 0,
+    })
+  }
+
+ 
+  return (
+    <>
+      <h1>Main value: {value.value}</h1>
+      <button
+      onClick={clickMe}>Click to multiply by 5</button>
+      {/* <h2>Multilplied value: {multipliedValue}</h2> */}
+    </>
+  )
+}
+
+export default App
